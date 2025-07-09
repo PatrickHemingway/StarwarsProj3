c66e43eb5dce97ac0b9840ddeb6cf056216162a0
Patrick Hemingway <patrickhemingway.work@gmail.com>
2025-07-09 14:54:53 -0400

    Animation

--- React/src/components/NavBar.jsx
+++ React/src/components/NavBar.jsx
@@ -1,17 +1,54 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import styles from './NavBar.module.css';
-import StarWarsLogo from '/logo.png'; 
 import SidebarMenu from './SideBarMenu';
 
 export default function NavBar() {
+  useEffect(() => {
+  const canvas = document.getElementById('nav-canvas');
+  const ctx = canvas.getContext('2d');
+  const fighter = new Image();
+  fighter.src = '/Fighter.png';
+
+  let x = 0;
+  let prevY = 0;
+  let frame = 0;
+  const speed = 3;
+
+  const animate = () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const y = canvas.height / 2 + Math.sin(frame * 0.1) * 20;
+    const dy = y - prevY;
+    const dx = speed;
+    const angle = Math.atan2(dy, dx); // angle in radians
+
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(angle);
+    ctx.drawImage(fighter, -30, -15, 60, 30); // center image
+    ctx.restore();
+
+    prevY = y;
+    x += speed;
+    if (x > canvas.width + 60) x = -60;
+
+    frame++;
+    requestAnimationFrame(animate);
+  };
+
+  fighter.onload = animate;
+}, []);
+
+
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
         <SidebarMenu />
-        {/* <Link to="/">
-          <img src={StarWarsLogo} alt="Home" />
-        </Link> */}
       </div>
+      <canvas id="nav-canvas" className={styles.canvas}></canvas>
     </div>
   );
 }
